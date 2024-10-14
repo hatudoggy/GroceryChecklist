e770d80c8e9b45a566d8cd0665e0469feceb7657
Harvey Dela Flor <harvey.delaflor@gmail.com>
2024-10-14 06:15:20 +0000

    Item Category and Item Tag Component

---
+++ app/src/main/java/com/example/grocerychecklist/ui/component/ItemCategory.kt
@@ -0,0 +1,11 @@
+import androidx.compose.ui.graphics.Color
+
+enum class ItemCategory(val color: Color, val text: String) {
+    POULTRY(Color(0xFFF3884B), "Poultry"),
+    MEAT(Color(0xFFF0CD52), "Meat"),
+    FRUIT(Color(0xFFAF64EA), "Fruit"),
+    MEDICINE(Color(0xFF6B79F8), "Medicine"),
+    VEGETABLE(Color(0xFF5BCBE3), "Vegetable"),
+    SANITARY(Color(0xFF8FE35B), "Sanitary"),
+    CLEANING(Color(0xFFF255A0), "Cleaning")
+}
