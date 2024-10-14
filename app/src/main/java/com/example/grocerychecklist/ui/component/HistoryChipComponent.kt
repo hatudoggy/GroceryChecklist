395374d627576ed8b1f948ebd0e1085a3fe95226
Harvey Dela Flor <harvey.delaflor@gmail.com>
2024-10-14 06:26:24 +0000

    Create HistoryChipComponent.kt

---
+++ app/src/main/java/com/example/grocerychecklist/ui/component/HistoryChipComponent.kt
@@ -0,0 +1,9 @@
+package com.example.grocerychecklist.ui.component
+
+import ItemCategory
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HistoryChipComponent(category: ItemCategory) {
+    Chip
+}
