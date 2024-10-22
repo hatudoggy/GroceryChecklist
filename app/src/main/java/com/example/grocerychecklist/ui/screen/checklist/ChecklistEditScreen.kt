697d35a2161ea2f006fee8957b9357be961f8ab1
RRDizon <162077601+RRDizon@users.noreply.github.com>
2024-10-22 09:34:22 +0800

    Update ChecklistEditScreen.kt

--- app/src/main/java/com/example/grocerychecklist/ui/screen/checklist/ChecklistEditScreen.kt
+++ app/src/main/java/com/example/grocerychecklist/ui/screen/checklist/ChecklistEditScreen.kt
@@ -1,26 +1,70 @@
 package com.example.grocerychecklist.ui.screen.checklist
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Android
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.grocerychecklist.ui.component.ChecklistComponent
+import com.example.grocerychecklist.ui.component.ChecklistComponentVariant
+import com.example.grocerychecklist.ui.component.ChecklistItemComponent
+import com.example.grocerychecklist.ui.component.ChecklistItemComponentVariant
+import com.example.grocerychecklist.ui.component.RoundedTextField
+import com.example.grocerychecklist.ui.component.TopBarComponent
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 @Composable
-fun ChecklistEditScreen() {
-    Scaffold {
-            innerPadding ->
+fun ChecklistViewScreen() {
+    Scaffold(
+        modifier = Modifier.padding(vertical = 0.dp),
+        contentWindowInsets = WindowInsets(0.dp),
+        topBar = { TopBarComponent(title = "Checklist") },
+    ) { innerPadding ->
         Column (
-            modifier = Modifier.padding(innerPadding)
+            modifier = Modifier
+                .padding(innerPadding)
+                .padding(10.dp)
         ) {
-
+            Column(
+                modifier = Modifier
+                    .padding(horizontal = 0.dp)
+            ) {
+                Text("Main Groceries", fontSize = 18.sp)
+                repeat(3) {
+                    ChecklistComponent(
+                        name = "Chicken",
+                        expense = 400.00,
+                        date = LocalDate.now().format(DateTimeFormatter.ofPattern("MMM dd yyyy")),
+                        icon = Icons.Filled.Fastfood,
+                        iconColor = MaterialTheme.colorScheme.primary,
+                        variant = ChecklistComponentVariant.History,
+                    )
+                }
+            }
         }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun ChecklistEditScreenPreview() {
-    ChecklistEditScreen()
+fun ChecklistViewScreenPreview() {
+    ChecklistViewScreen()
 }
