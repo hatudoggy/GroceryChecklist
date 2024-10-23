eda3314ba5904f315436e60180e4a8d92cf4046c
Kristopher Santos <topers1820@gmail.com>
2024-10-23 14:44:00 +0800

    Commented imports

--- app/src/main/java/com/example/grocerychecklist/ui/screen/checklist/ChecklistViewScreen.kt
+++ app/src/main/java/com/example/grocerychecklist/ui/screen/checklist/ChecklistViewScreen.kt
@@ -34,8 +34,8 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.grocerychecklist.ui.component.ChecklistComponent
-import com.example.grocerychecklist.ui.component.ChecklistComponentVariant
+//import com.example.grocerychecklist.ui.component.ChecklistComponent
+//import com.example.grocerychecklist.ui.component.ChecklistComponentVariant
 import com.example.grocerychecklist.ui.component.ChecklistItemComponent
 import com.example.grocerychecklist.ui.component.ChecklistItemComponentVariant
 import com.example.grocerychecklist.ui.component.RoundedTextField
