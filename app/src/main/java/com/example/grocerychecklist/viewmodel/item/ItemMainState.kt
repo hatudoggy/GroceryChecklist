a9768134a325c77baa8bf89415424c90aa6630c7
Ibra <lr.idesoukyharby@mmdc.mcl.edu.ph>
2025-02-10 18:42:18 +0800

    feat: Add selectedItem to ItemMainState for edit functionality
    - Introduced selectedItem to track the currently selected item.
    - Enables item editing by storing the selected item's details.

--- app/src/main/java/com/example/grocerychecklist/viewmodel/item/ItemMainState.kt
+++ app/src/main/java/com/example/grocerychecklist/viewmodel/item/ItemMainState.kt
@@ -4,5 +4,6 @@ import com.example.grocerychecklist.data.model.Item
 
 data class ItemMainState (
     val isAddingItem: Boolean = false,
-    val items: List<Item> = emptyList()
+    val items: List<Item> = emptyList(),
+    val selectedItem: Item? = null
 )
