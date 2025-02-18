3cb90f9961977d056a77ed2a9698345a75be29da
Ibra <lr.idesoukyharby@mmdc.mcl.edu.ph>
2025-02-18 12:34:18 +0800

    Enhance ChecklistDetailViewModel with Repository & State Management
    - Injected ChecklistRepository for fetching checklist details.
    - Introduced MutableStateFlow to manage and expose UI state.
    - Implemented loadChecklistDetails() to fetch checklist data asynchronously.
    - Handled loading, success, and error states for improved UI responsiveness.

--- app/src/main/java/com/example/grocerychecklist/viewmodel/checklist/ChecklistDetailViewModel.kt
+++ app/src/main/java/com/example/grocerychecklist/viewmodel/checklist/ChecklistDetailViewModel.kt
@@ -1,16 +1,40 @@
 package com.example.grocerychecklist.viewmodel.checklist
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import androidx.navigation.NavBackStackEntry
 import androidx.navigation.toRoute
+import com.example.grocerychecklist.data.repository.ChecklistRepository
 import com.example.grocerychecklist.ui.screen.Navigator
 import com.example.grocerychecklist.ui.screen.Routes
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
 class ChecklistDetailViewModel(
+    private val checklistRepository: ChecklistRepository,
     private val navigator: Navigator,
     entry: NavBackStackEntry
 ): ViewModel() {
+
+    private val _state = MutableStateFlow<ChecklistDetailState>(ChecklistDetailState.Loading)
+    val state: StateFlow<ChecklistDetailState> = _state
     val checklistId = entry.toRoute<Routes.ChecklistDetail>().checklistId
+
+    init {
+        loadChecklistDetails()
+    }
+
+    private fun loadChecklistDetails() {
+        viewModelScope.launch {
+            try {
+                val checklist = checklistRepository.getChecklist(checklistId)
+                _state.value = ChecklistDetailState.Loaded(checklist)
+            } catch (_: Exception) {
+                _state.value = ChecklistDetailState.Error
+            }
+        }
+    }
 
     fun onEvent(event: ChecklistDetailEvent) {
         when (event) {
