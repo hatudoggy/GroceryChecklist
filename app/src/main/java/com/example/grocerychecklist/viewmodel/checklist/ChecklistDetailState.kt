631002050f149e947fcb28d42fe842dd6ffd635d
Ibra <lr.idesoukyharby@mmdc.mcl.edu.ph>
2025-02-18 12:35:00 +0800

    Define ChecklistDetailState with Checklist Model Dependency

--- app/src/main/java/com/example/grocerychecklist/viewmodel/checklist/ChecklistDetailState.kt
+++ app/src/main/java/com/example/grocerychecklist/viewmodel/checklist/ChecklistDetailState.kt
@@ -1,5 +1,9 @@
 package com.example.grocerychecklist.viewmodel.checklist
 
-sealed class ChecklistDetailState {
+import com.example.grocerychecklist.data.model.Checklist
 
+sealed class ChecklistDetailState {
+        data object Loading : ChecklistDetailState()
+        data class Loaded(val checklist: Checklist) : ChecklistDetailState()
+        data object Error : ChecklistDetailState()
 }
