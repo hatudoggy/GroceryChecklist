944a6cfe37f6fb29391dac4c176ac3ea05270a2f
Morfusee <markvalenzuela.coding@gmail.com>
2025-03-27 00:54:18 +0800

    FEAT: Implemented Item Repository Firestore DAO and Impl

--- app/src/main/java/com/example/grocerychecklist/data/dao/firestoreImpl/FItemDAOImpl.kt
+++ app/src/main/java/com/example/grocerychecklist/data/dao/firestoreImpl/FItemDAOImpl.kt
@@ -1,53 +1,85 @@
 package com.example.grocerychecklist.data.dao.firestoreImpl
 
 import com.example.grocerychecklist.data.dao.ItemDAO
-import com.example.grocerychecklist.data.model.ChecklistItem
+import com.example.grocerychecklist.data.dto.ItemFirestore
 import com.example.grocerychecklist.data.model.Item
 import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.snapshots
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.tasks.await
 
-class FItemDAOImpl: FBaseIUDDAOImpl<Item>(
+class FItemDAOImpl : FBaseIUDDAOImpl<Item>(
     FirestoreCollections.ITEMS
 ), ItemDAO {
 
     override suspend fun getItemById(itemId: Long): Item {
-        TODO("Not yet implemented")
+        val query = db.whereEqualTo("id", itemId).get().await()
+        val snapshot = query.documents.firstOrNull()
+            ?: throw NoSuchElementException("Checklist with id $itemId not found")
+
+        return fromFirestoreModel(snapshot)
     }
 
     override fun getAllItems(): Flow<List<Item>> {
-        TODO("Not yet implemented")
+        return db.snapshots().map { querySnapshot ->
+            querySnapshot.documents.map { fromFirestoreModel(it) }
+        }
     }
 
     override fun getAllItemsOrderedByName(): Flow<List<Item>> {
-        TODO("Not yet implemented")
+        return db.orderBy("name").snapshots().map { querySnapshot ->
+            querySnapshot.documents.map { fromFirestoreModel(it) }
+        }
     }
 
     override fun getAllItemsOrderedByPrice(): Flow<List<Item>> {
-        TODO("Not yet implemented")
+        return db.orderBy("price").snapshots().map { querySnapshot ->
+            querySnapshot.documents.map { fromFirestoreModel(it) }
+        }
     }
 
     override fun getAllItemsOrderedByCreatedAt(): Flow<List<Item>> {
-        TODO("Not yet implemented")
+        return db.orderBy("createdAt").snapshots().map { querySnapshot ->
+            querySnapshot.documents.map { fromFirestoreModel(it) }
+        }
     }
 
     override fun getAllItemsByName(qName: String): Flow<List<Item>> {
-        TODO("Not yet implemented")
+        return db.whereEqualTo("name", qName).snapshots().map { querySnapshot ->
+            querySnapshot.documents.map { fromFirestoreModel(it) }
+        }
     }
 
     override fun getAllItemsByCategory(category: String): Flow<List<Item>> {
-        TODO("Not yet implemented")
+        return db.whereEqualTo("category", category).snapshots().map { querySnapshot ->
+            querySnapshot.documents.map { fromFirestoreModel(it) }
+        }
     }
 
     override suspend fun deleteItemById(itemId: Long): Int {
-        TODO("Not yet implemented")
+        val querySnapshot = db.whereEqualTo("id", itemId).get().await()
+
+        if (querySnapshot.isEmpty) {
+            throw NoSuchElementException("Item with id $itemId not found")
+        }
+
+        querySnapshot.documents.forEach {
+            db.document(it.reference.path).delete().await()
+        }
+
+        return querySnapshot.size()
     }
 
     override fun toFirestoreModel(obj: Item): Map<String, Any?> {
-        TODO("Not yet implemented")
+        return ItemFirestore.fromItem(obj).toMap()
     }
 
     override fun fromFirestoreModel(snapshot: DocumentSnapshot): Item {
-        TODO("Not yet implemented")
+        val doc = snapshot.toObject(ItemFirestore::class.java)
+            ?: throw IllegalStateException("Failed to parse item data.")
+
+        return doc.toItem()
     }
 
 }
---
+++ app/src/main/java/com/example/grocerychecklist/data/dto/ItemFirestore.kt
@@ -0,0 +1,78 @@
+package com.example.grocerychecklist.data.dto
+
+import com.example.grocerychecklist.data.model.Item
+import com.google.firebase.Timestamp
+import com.google.firebase.firestore.PropertyName
+
+class ItemFirestore (
+    @get:PropertyName("id") @set:PropertyName("id")
+    var id: Long = 0,
+
+    @get:PropertyName("name") @set:PropertyName("name")
+    var name: String = "",
+
+    @get:PropertyName("price") @set:PropertyName("price")
+    var price: Double = 0.0,
+
+    @get:PropertyName("category") @set:PropertyName("category")
+    var category: String = "",
+
+    @get:PropertyName("measureType") @set:PropertyName("measureType")
+    var measureType: String = "",
+
+    @get:PropertyName("measureValue") @set:PropertyName("measureValue")
+    var measureValue: Double = 0.0,
+
+    @get:PropertyName("photoRef") @set:PropertyName("photoRef")
+    var photoRef: String = "",
+
+    @get:PropertyName("createdAt") @set:PropertyName("createdAt")
+    var createdAt: Timestamp? = null,
+
+    @get:PropertyName("updatedAt") @set:PropertyName("updatedAt")
+    var updatedAt: Timestamp? = null,
+) {
+    fun toItem(): Item {
+        return Item(
+            id = id,
+            name = name,
+            price = price,
+            category = category,
+            measureType = measureType,
+            measureValue = measureValue,
+            photoRef = photoRef,
+            createdAt = createdAt?.toLocalDateTime(),
+            updatedAt = updatedAt?.toLocalDateTime()
+        )
+    }
+
+    companion object {
+        fun fromItem(item: Item): ItemFirestore {
+            return ItemFirestore(
+                id = item.id,
+                name = item.name,
+                price = item.price,
+                category = item.category,
+                measureType = item.measureType,
+                measureValue = item.measureValue,
+                photoRef = item.photoRef,
+                createdAt = item.createdAt?.toTimestamp(),
+                updatedAt = item.updatedAt?.toTimestamp()
+            )
+        }
+    }
+
+    fun toMap(): Map<String, Any?> {
+        return mapOf(
+            "id" to id,
+            "name" to name,
+            "price" to price,
+            "category" to category,
+            "measureType" to measureType,
+            "measureValue" to measureValue,
+            "photoRef" to photoRef,
+            "createdAt" to createdAt,
+            "updatedAt" to updatedAt
+        )
+    }
+}
